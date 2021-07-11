d7d7448666dafebe321d7616dbd60f30581178d0
Christian Bawmann <christiangbm@hotmail.com>
2021-07-11 12:51:43 -0700

    remove hardcoded data

--- src/app.js
+++ src/app.js
@@ -11,19 +11,6 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({description: 'Water bill', amount: 500}));
-store.dispatch(addExpense({description: 'Gas bill', createdAt: 1000}));
-store.dispatch(addExpense({description: 'Rent', amount: 3400}));
-
-
-
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
-console.log(store.getState());
-
 const jsx = (
     <Provider store={store}>
         <AppRouter/>
